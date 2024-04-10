59f4faefcdd934ed067b9f41b3bc32674f4c9fbf
Arnav Jain <JainAR1@cardiff.ac.uk>
2024-04-10 13:13:39 +0100

    Created base code for sign up page

---
+++ community-hub/app/signUp/page.jsx
@@ -0,0 +1,10 @@
+export default function SignUpPage() {
+    return (
+        <section className="p-8 flex gap-4 flex-col">
+
+            <div className="p-4 bg-slate-600"></div>
+            
+
+        </section>
+    )
+}
