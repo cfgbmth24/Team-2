46c928585dd31c3b91fdd5fa5dd886b15c96ec4c
Ed <edward.blewitt2@gmail.com>
2024-04-10 12:37:15 +0100

    Homepage scaffold done

--- community-hub/app/layout.jsx
+++ community-hub/app/layout.jsx
@@ -4,11 +4,20 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <PocketProvider>
-        <body>
-          {children}
-        </body>
-      </PocketProvider>
+		<PocketProvider>
+			<body className="p-4 px-12">
+
+				<nav className="flex justify-between pb-8">
+
+					<img src="/logo.jpg" className="h-16" />
+
+					<button className="p-4 bg-pink text-white font-bold rounded-xl">Log in</button>
+
+				</nav>
+
+				{children}
+			</body>
+		</PocketProvider>
     </html>
   );
 }
--- community-hub/app/page.jsx
+++ community-hub/app/page.jsx
@@ -1,9 +1,41 @@
+'use client'
+
 export default function HomePage() {
     return (
-        <section>
-            <h1 className="text-4xl">Welcome to the app</h1>
+        <section className="flex gap-6">
 
-            Time to learn React
+            {/* Left col             */}
+            <div className="flex-1 flex flex-col gap-6">
+
+                <div className="p-6 bg-green-300">
+                    <h1>Stats herez</h1>
+                    <p>Community size</p>
+                </div>
+
+                <div className="p-6 bg-green-300">
+                    Revirews
+                </div>
+
+                <div className="p-6 bg-green-300">
+                    Events list
+                </div>
+
+            </div>
+
+
+            {/* Right col */}
+            <div className="w-72 flex flex-col gap-8">
+
+                <div className="p-6 bg-green-300">
+                    <h1>Leaderboard</h1>
+                </div>
+
+                <div className="p-6 bg-green-300">
+                    Your tickets
+                </div>
+
+            </div>
+
         </section>
     )
 }
