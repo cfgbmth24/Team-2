bb72ec16a9d0805beffe27f3321804c3422d8ed7
Amandi Sevmini Edirisinghe <eeyae8@nottingham.ac.uk>
2024-04-10 14:40:07 +0100

    commit the event list

--- community-hub/app/page.jsx
+++ community-hub/app/page.jsx
@@ -1,6 +1,24 @@
 'use client'
 
+import { usePocket } from "@/contexts/pocketContext.jsx"
+import { useEffect, useState } from "react"
+
 export default function HomePage() {
+
+    const { pb } = usePocket()
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        pb.collection('events').getFullList({
+            sort: '-startTime',
+        })
+        .then(res => {
+            console.log(res)
+            setEvents(res)
+        })
+    }, [])
+
+    
     return (
         <section className="flex gap-6">
 
@@ -16,9 +34,18 @@ export default function HomePage() {
                     Revirews
                 </div>
 
-                <div className="p-6 bg-green-300">
-                    Events list
+                <div className="grid grid-cols-3 gap-6">
+                    {
+                        events?.map((event, i) => {
+                            return (
+                                <div key={i} className="p-6 bg-gray-500 shadow-lg rounded-xl gap-6">
+                                    <h1>{event?.name}</h1>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
+
 
             </div>
 
