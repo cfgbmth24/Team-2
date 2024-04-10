1b1f6a09a1e058b1c56b50946913bd5b7bbc648b
AndyKaspe07 <andy.kaspe07@gmail.com>
2024-04-10 13:33:30 +0100

    Title added

--- community-hub/app/review/page.jsx
+++ community-hub/app/review/page.jsx
@@ -1,10 +1,12 @@
 export default function HomePage() {
     return (
+        <body className="p-10 px-12">
 
-        <div>
-            Hello World
+        <div className="px-10 py-6 bg-gray-200" style={{ height: '2000px' }}>
+
+        <h1 className="text-lg font-bold mb-4 text-center">Review Form</h1>
+        This is a box with 10px padding on each side.
         </div>
-
-
+        </body>
     )
 }
