c2ee265fa163a92de1b424bf751184fc303faf53
Ed <edward.blewitt2@gmail.com>
2024-04-10 11:41:22 +0100

    Connects to the real backend

--- community-hub/app/layout.jsx
+++ community-hub/app/layout.jsx
@@ -1,13 +1,11 @@
 import { PocketProvider } from "@/contexts/pocketContext";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <PocketProvider>
-        <body className={inter.className}>
+        <body>
           {children}
         </body>
       </PocketProvider>
---
+++ community-hub/app/page.jsx
@@ -0,0 +1,9 @@
+export function HomePage() {
+    return (
+        <section>
+            <h1 className="text-4xl">Welcome to the app</h1>
+
+            Time to learn React
+        </section>
+    )
+}
